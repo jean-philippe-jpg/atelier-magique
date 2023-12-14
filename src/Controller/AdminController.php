3aecfe2d6f7c170b7fae2b6bec61d5f3c46f0307
champion <jphilippe.champion@gmail.com>
2023-12-14 10:06:04 +0100

    formulaire de contact

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ElfesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        return $this->render('elfes/index.html.twig');
+    }
+}
