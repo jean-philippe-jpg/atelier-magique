417d86647db8294c4051e7f3580e66d406084aeb
champion <jphilippe.champion@gmail.com>
2023-12-15 15:00:27 +0100

    systeme de commentaires

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
+use App\Repository\CommentaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +14,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+            // systeme de commentaire
+            $commentaire = new Commentaire;
+
+            // formulaire
+            $commentaireForm = $this->createForm( CommentaireType::class, $commentaire);
+            $commentaireForm->handleRequest($request);
+
+            if ($commentaireForm->isSubmitted() && $commentaireForm->isValid()){
+
+                
+                $entityManager->persist($commentaire);
+                $entityManager->flush();
+                
+
+                $this->addFlash('message', 'je te souhaite un joyeux noel');
+                return $this->redirectToRoute('app_accueil_index', [], Response::HTTP_SEE_OTHER);
+            }
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'commentaireForm' => $commentaireForm->createView(),
         ]);
     }
+
 }
